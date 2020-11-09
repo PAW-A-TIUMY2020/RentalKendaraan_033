f480ff3dc3ab7568a24795ceaf219280527ae9b5
Elvira Yolanda <elvirayolanda828@gmail.com>
2020-11-09 20:25:05 +0700

    add code from modul 5

--- RentalKendaraan_033/Models/JenisKendaraan.cs
+++ RentalKendaraan_033/Models/JenisKendaraan.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan_033.Models
 {
     public partial class JenisKendaraan
     {
         public int IdJenisKendaraan { get; set; }
+
+        [Required(ErrorMessage = "Nama Jenis Kendaraan wajib diisi")]]
         public string NamaJenisKendaraan { get; set; }
     }
 }
---
+++ RentalKendaraan_033/Models/KondisiKendaraan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentalKendaraan_033.Models
+{
+    public partial class KondisiKendaraan
+    {
+        public int IdKondisi { get; set; }
+
+        [Required(ErrorMessage = "Nama Kondisi wajib diisi")]
+        public string NamaKondisi { get; set; }
+    }
+}
